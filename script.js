5f3fb1c6bbd8673cc23e59386cb4124cf9cc51aa
lewarrior <101592967+lewarrior@users.noreply.github.com>
2024-05-08 19:41:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuAMD = document.querySelector(".b3")
+const navlink = document.querySelector(".n1")
+
+menuAMD.addEventListener('click', ()=>{
+    navlink.classList.toggle('mobile-menu')
+})
